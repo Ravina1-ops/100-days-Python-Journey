9a7b65fa099f2d102c785104a67cddfdbb77132d
Ravina <hema130384@gmail.com>
2026-01-12 15:35:37 +0530

    Operators Program

---
+++ 4thDay.py
@@ -0,0 +1,11 @@
+# Operators = + - / * all these are operators
+a = 12 
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #float module operator means if ans is 2.5 after using // it gives only 2
+print(a%b)
+print(2**3) # use to raise power
+
