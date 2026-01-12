4fadef7d41dba35549304c448d14863105aa8a50
Ravina <hema130384@gmail.com>
2026-01-12 16:13:34 +0530

    Calculator Exercise 1

---
+++ 5thDay_Exercise_1.py
@@ -0,0 +1,17 @@
+# Create a calculator which can perform addition subtraction multiplication division on two numbers
+a = 35
+b = 6
+c = a + b 
+d = a - b
+e = a * b
+f = a / b
+g = a // b
+h = a % b 
+i = a ** b
+print("Addition of ",a,"and",b,"is = ",c)
+print("Subtraction of ",a,"and",b,"is = ",d)
+print("Multiplication of ",a,"and",b,"is = ",e)
+print("Division of ",a,"and",b,"is = ",f)
+print("Floor Division of ",a,"and",b,"is = ",g)
+print("Modulus of ",a,"and",b,"is = ",h)
+print(b, "raise to ",a ,"is = ",i)
