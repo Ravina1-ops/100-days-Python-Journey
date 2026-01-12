adac3ae8559433b8b65454ba326f52bc9ece5e4c
Ravina <hema130384@gmail.com>
2026-01-12 15:26:49 +0530

    Variable and Data types program

--- 3rdDay.py
+++ 3rdDay.py
@@ -1,2 +1,19 @@
 # Variables and Data Types 
- 
+''' variable are containers which holds data and data type is type of data stored '''
+a = 5 #initialize a variable a which store a value
+print(a,type(a)) #integer data type
+b = complex(8,3)
+print(b, type(b)) 
+c = True
+print(c , (type(c))) #type(c) tell the data type it is in built function of python 
+d = "Ravina"
+print(d,(type(d)))
+
+# Data types in python = int boolean string list tuple and dict 
+# everthing in python is object 
+g = 87
+print(a + g ,(type(g)))
+h= " Saini"
+print(d + h)
+
+# Operation will perform on same data type
