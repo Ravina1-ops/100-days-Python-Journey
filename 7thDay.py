d7afba0f09f23322981bb8b188424f5ae5e3167a
Ravina <hema130384@gmail.com>
2026-01-13 21:29:27 +0530

    input from user program

---
+++ 7thDay.py
@@ -0,0 +1,14 @@
+# Taking input from user 
+# every input consider as string 
+# /to perform arithmetic operation typecaste that one 
+
+a = input("Enter your name: ")
+print("My name is : ",a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+print( x + y ) # give xy not mathematical operation answer 
+
+print( int(x) + int(y)) # typecasting is done to add them 
+
