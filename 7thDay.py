b2b7d84bb57dd9eccb57b32e2012dbb6b482dfb5
Ravina <hema130384@gmail.com>
2026-01-13 21:43:15 +0530

    updating calculator using input function

--- 7thDay.py
+++ 7thDay.py
@@ -12,3 +12,21 @@ print( x + y ) # give xy not mathematical operation answer
 
 print( int(x) + int(y)) # typecasting is done to add them 
 
+''' calculator using user input'''
+
+b = input("Enter first number = ")
+d = input("Enter second number = ")
+c = int(b) + int(d)
+k = int(b) - int(d)
+e = int(b) * int(d)
+f = int(b) / int(d)
+g = int(b) // int(d)
+h = int(b) % int(d)
+i = int(b) ** int(d)
+print("Addition of ",b,"and",d,"is = ",c)
+print("Subtraction of ",b,"and",d,"is = ",k)
+print("Multiplication of ",b,"and",d,"is = ",e)
+print("Division of ",b,"and",d,"is = ",f)
+print("Floor Division of ",b,"and",d,"is = ",g)
+print("Modulus of ",b,"and",d,"is = ",h)
+print(d, "raise to ",b ,"is = ",i)
