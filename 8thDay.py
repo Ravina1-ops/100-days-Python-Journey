0048760900bd2db26e432f57a60b4bd08a5bdfb0
Ravina <hema130384@gmail.com>
2026-01-14 21:38:50 +0530

    String program

---
+++ 8thDay.py
@@ -0,0 +1,21 @@
+# String
+name = "Ravina"
+friend = 'Muskan' # string can be written in double and single quotes
+print(name)
+print(friend)
+
+# for multiple line string triple single or double quotes are used
+
+apple = """An apple is a very Good fruit.
+You should definitely eat one apple in a Day.
+As according to a idiom it is said that
+'One apple a Day Keeps you doctor away. """
+
+print(apple)
+
+# if you want to print character by index then for loop is used
+
+for character in name:
+    print(character)
+
+   
