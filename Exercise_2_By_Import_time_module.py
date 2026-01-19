720017099fe3fe4bc44002b7f0d39b0c8aca827f
Ravina <hema130384@gmail.com>
2026-01-19 18:51:28 +0530

    Greeting using import function

---
+++ Exercise_2_By_Import_time_module.py
@@ -0,0 +1,12 @@
+import time
+timestamp = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+print(timestamp)
+print(hour)
+if(hour>0 and hour<12):
+    print("Good morning.")
+elif (hour== 12 and hour <16) :
+    print("Good afternoon.")
+else :
+    print("Good Evening.")
+    
