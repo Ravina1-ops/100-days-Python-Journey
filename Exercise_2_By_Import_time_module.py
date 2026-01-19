581ce1e7ff6eda4489b7103a571125cae378ad61
Ravina <hema130384@gmail.com>
2026-01-19 18:53:51 +0530

    Greeting using import function

--- Exercise_2_By_Import_time_module.py
+++ Exercise_2_By_Import_time_module.py
@@ -2,6 +2,7 @@ import time
 timestamp = time.strftime('%H:%M:%S')
 hour = int(time.strftime('%H'))
 print(timestamp)
+hour = int(input("Enter Hour: "))
 print(hour)
 if(hour>0 and hour<12):
     print("Good morning.")
