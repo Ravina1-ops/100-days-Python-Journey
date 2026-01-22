c6a3aacc8fad8ff976195e73d8cf0bd4019eb7ab
Ravina <hema130384@gmail.com>
2026-01-22 21:54:31 +0530

    Multiplication table and calender program

--- 17th_Day.py
+++ 17th_Day.py
@@ -3,3 +3,7 @@ year = int(input("Enter year: "))
 month = int(input("Enter month: "))
 cal = calendar.month(year, month)
 print(cal)
+
+num = int(input("Display multiplication table of: "))
+for i in range(1, 11):
+ print(f"{num} X {i} = {num*i}")
