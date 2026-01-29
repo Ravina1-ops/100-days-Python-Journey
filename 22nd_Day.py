a043f77a6d0d4a8262c09acd6cd2cb4a96764ce9
Ravina <hema130384@gmail.com>
2026-01-29 14:19:06 +0530

    Tuple introduction

---
+++ 22nd_Day.py
@@ -0,0 +1,16 @@
+# Tuples are ordered collection if data items They store multiple items in
+# a single variable. Tuple items are separated by commas and enclosed
+#  within () Tuples are unchangable 
+
+tup = (5,6,78,34,58,96,123,147,52)
+print(type(tup))
+print(tup)
+print("Length of tuple: ",len(tup))
+
+# all concepts are same as list in tuple just we cant change tuple
+
+tup2 = tup[1:5]
+print(tup2)
+
+print(tup[8])
+# accessing element by using index
