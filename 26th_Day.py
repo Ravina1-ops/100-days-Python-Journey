38b8c13850b7cdf736a738153e215512405b7279
Ravina <hema130384@gmail.com>
2026-02-02 19:07:25 +0530

    Doc-String intro

---
+++ 26th_Day.py
@@ -0,0 +1,25 @@
+''' Doc string are string literals that apppear right after
+ the definition of a function ,method ,class or module'''
+
+def square(n):
+    '''Take a number n and return its square value'''
+    print(n**2)
+square(7)
+print(square.__doc__)
+
+# above is the syntax to print doc string of a function
+
+"""comments or docstring me main difference
+hai ki docstring humesha function
+ki next line me likhi jayagi otherwise vo ak comment 
+consider kiya jayaga """
+
+def square2(n):
+    print(n)
+    '''Take a number n and return its 
+    square value'''
+    print(n**2)
+square2(7)
+print(square2.__doc__)
+# iski docstring none hogi kyoki ye just after func nhi likhi hai
+
