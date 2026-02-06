7964e590a50726225392e089fd380cab2b4b9345
Ravina <hema130384@gmail.com>
2026-02-06 08:12:51 +0530

    fibonacci series program

--- 28th_Day.py
+++ 28th_Day.py
@@ -25,3 +25,17 @@ def fibonacci(m):
 print(fibonacci(4))
 print(fibonacci(5))
 print(fibonacci(7))
+
+def recur_fibo(n):
+ if n <= 1:
+  return n
+ else:
+  return(recur_fibo(n-1) + recur_fibo(n-2))
+nterms = int(input("Enter the number of terms (greater than 0): "))
+# check if the number of terms is valid
+if nterms <= 0:
+ print("Plese enter a positive integer")
+else:
+ print("Fibonacci sequence:")
+for i in range(nterms):
+ print(recur_fibo(i))
