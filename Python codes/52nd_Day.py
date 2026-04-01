1cf4da9497b0ca72513355f79d39bdb7b7f733cf
Ravina <hema130384@gmail.com>
2026-04-01 15:31:48 +0530

    decorators function in python

---
+++ Python codes/52nd_Day.py
@@ -0,0 +1,26 @@
+# decorators in python = basically used for modifying a function taking that as an argument
+
+def greet(fx):
+    def mfx():
+         print("According to question statement")
+         fx()
+         print("Here we solved the question")
+    return mfx
+
+# here by using @greet decorator we modify the function math and mul by adding two statements in both 
+
+@greet
+def math():
+    x = 7
+    y = 9
+    print(f"x = {x} and y = {y} Add = {x+y}")
+
+@greet
+def mul():
+    a = 5
+    b = 4
+    print(f"x = {a} and y = {b} multiply = {a*b}")
+
+
+math()
+mul()
