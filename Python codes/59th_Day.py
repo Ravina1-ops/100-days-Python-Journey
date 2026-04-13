1112a4fa5bfd661f25fb94a6ff91b7748c0032ef
Ravina <hema130384@gmail.com>
2026-04-13 11:30:23 +0530

    instance varibale code

--- Python codes/59th_Day.py
+++ Python codes/59th_Day.py
@@ -1,2 +1,9 @@
 # instances and class variable
 
+class Employee:
+    def __init__(self, name):
+        self.name = name
+    def showDetails(self):
+        print("The name of the Employee is {self.name}")
+
+emp1 = Employee
