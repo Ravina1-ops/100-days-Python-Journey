6e384e1cc73c9547111321351150744260c48858
Ravina <hema130384@gmail.com>
2026-04-15 19:35:27 +0530

    solution of 8th exercise

--- Python codes/73rd_Day_Exercise_8th.py
+++ Python codes/73rd_Day_Exercise_8th.py
@@ -8,3 +8,16 @@ pypdf is a free and open-source pure-python PDF library capable of splitting,
 merging, cropping, and transforming the pages of PDF files.
 '''
 # Note: You will need to pip install pypdf
+
+#  - Exercise 8 Solution
+from pypdf import PdfWriter
+import os
+
+merger = PdfWriter()
+files = [file for file in os.listdir() if file.endswith(".pdf")]
+
+for pdf in files:
+    merger.append(pdf)
+
+merger.write("merged-pdf.pdf")
+merger.close()
