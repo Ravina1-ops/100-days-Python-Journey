73e28dd3e16d5208300af6d0f9c703ce61d2fa97
Ravina <hema130384@gmail.com>
2026-05-02 19:47:40 +0530

    edit complete code with errors

--- Python codes/78th_Day.py
+++ Python codes/78th_Day.py
@@ -12,13 +12,13 @@
 
 def mean(n):
 # Convert the number to a string to iterate through its digits
-n_str = str(n)
+    n_str = str(n)
 # Calculate the sum of digits
-digit_sum = sum(int(digit) for digit in n_str)
+    digit_sum = sum(int(digit) for digit in n_str)
 # Calculate the mean by dividing the sum by the number of digits
-digit_count = len(n_str)
-digit_mean = digit_sum / digit_count
-return int(digit_mean) 
-In [216]:
-1
+    digit_count = len(n_str)
+    digit_mean = digit_sum / digit_count
+    return int(digit_mean) 
+
+
 mean(42)
