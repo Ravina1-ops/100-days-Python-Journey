c4e422d9042afff15cf517ef5fba20deab1c36ec
Ravina <hema130384@gmail.com>
2026-05-02 19:42:46 +0530

    adding comment

--- Python codes/78th_Day.py
+++ Python codes/78th_Day.py
@@ -1,28 +1,15 @@
-Create a function that returns the mean of all digits.
-Examples
-mean(42) 
-➞ 3
-mean(12345) 
-➞ 3
-mean(666) 
-➞ 6
-Notes
-In [215]:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-The mean of all digits is the sum of digits / how many digits there are (e.g. mean
-of digits in 512 is (5+1+2)/3(number of digits) = 8/3=2).
-The mean will always be an integer.
+# Create a function that returns the mean of all digits.
+# Examples
+# mean(42) 
+# ➞ 3
+# mean(12345) 
+# ➞ 3
+# mean(666) 
+# ➞ 6
+# The mean of all digits is the sum of digits / how many digits there are (e.g. mean
+# of digits in 512 is (5+1+2)/3(number of digits) = 8/3=2).
+# The mean will always be an integer.
+
 def mean(n):
 # Convert the number to a string to iterate through its digits
 n_str = str(n)
