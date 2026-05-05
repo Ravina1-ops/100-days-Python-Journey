5ce58c66488076ddea4c8cd634440450dbf32fad
Ravina <hema130384@gmail.com>
2026-05-05 18:02:19 +0530

    comment the question area

--- Python codes/79th_Day.py
+++ Python codes/79th_Day.py
@@ -1,21 +1,14 @@
-Create a function that sorts a list and removes all duplicate items from it.
-Examples
-setify([1, 3, 3, 5, 5]) 
-setify([4, 4, 4, 4]) 
-➞ [1, 3, 5]
-➞ [4]
-setify([5, 7, 8, 9, 10, 15]) 
-➞ [5, 7, 8, 9, 10, 15]
-setify([3, 3, 3, 2, 1]) 
-➞ [1, 2, 3]
-In [210]:
-In [211]:
-1
-2
-3
-4
-5
-6
+# Create a function that sorts a list and removes all duplicate items from it.
+# Examples
+# setify([1, 3, 3, 5, 5]) 
+# setify([4, 4, 4, 4]) 
+# ➞ [1, 3, 5]
+# ➞ [4]
+# setify([5, 7, 8, 9, 10, 15]) 
+# ➞ [5, 7, 8, 9, 10, 15]
+# setify([3, 3, 3, 2, 1]) 
+# ➞ [1, 2, 3]
+
 def setify(lst):
 unique_set = set(sorted(lst))
 # Convert the set back to a list and return it
