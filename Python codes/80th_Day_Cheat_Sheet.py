37d8e1325eee636e1bbf9b10862cb357b162a504
Ravina <hema130384@gmail.com>
2026-05-06 20:11:18 +0530

    integer boolean dictonary tuple cheat sheet code

--- Python codes/80th_Day_Cheat_Sheet.py
+++ Python codes/80th_Day_Cheat_Sheet.py
@@ -12,3 +12,28 @@
 # my_string.find('l')
 # # replaces any instance of the first string with the second in my_string      
 # my_string.replace('H', 'C'
+
+
+Integer
+A whole number
+my_integer = 12321
+Float
+A decimal number
+my_decimal = 3.14
+Boolean
+Discrete value true or false
+a = True 
+b = False
+Dictionary
+Changeable collection of key-value pairs
+my_dictionary = {'banana': 1, 12: 'laptop', (0,0):'center'}
+Dictionary Operations
+# Access value using key
+my_dictionary['banana']
+# Get all keys in a dictionary as a list
+my_dictionary.keys()
+# Get all values in a dictionary as a list
+my_dictionary.values()
+Tuple
+Unchangeable collection of objects
+tup = (1, 3.12, False, "Hi")
