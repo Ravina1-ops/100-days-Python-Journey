512098cf05223cbedfdc4c637ca12e1fd3824e84
Ravina <hema130384@gmail.com>
2026-05-06 20:08:26 +0530

    adding cheat sheet of python comment

---
+++ Python codes/80th_Day.py
@@ -0,0 +1,14 @@
+# Python Data Types
+String
+Series of characters or data stored as text
+my_string = "Hello"
+String Operations
+# returns the string with all uppercase letters
+my_string.upper()
+# returns the length of a string
+len(my_string)
+# returns the index of the first instance of the string inside the 
+# subject string, otherwise -1
+my_string.find('l')
+# replaces any instance of the first string with the second in my_string      
+my_string.replace('H', 'C'
