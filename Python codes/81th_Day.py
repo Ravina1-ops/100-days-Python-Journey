825b22081aa1217f255dc1c8178293fd298f437b
Ravina <hema130384@gmail.com>
2026-05-12 08:45:10 +0530

    function define for solution

--- Python codes/81th_Day.py
+++ Python codes/81th_Day.py
@@ -2,13 +2,14 @@
 # 7, between a given range 0 and n.
 
 class DivisibleBySeven:
-def __init__(self,n):
-self.n = n
+    def __init__(self,n):
+        self.n = n
+
 def generate_divisible_by_seven(self):
-for num in range(self.n + 1):
-if num%7 == 0:
-yield num
-49/95
+    for num in range(self.n + 1):
+       if num%7 == 0:
+            yield num
+
 localhost:8888/notebooks/Piush Kumar Sharma/Basic Python Program.ipynb
 11/26/23, 4:53 AM
 Basic Python Program - Jupyter Notebook
