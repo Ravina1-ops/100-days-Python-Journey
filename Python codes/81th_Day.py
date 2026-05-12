25c72a2f8a9b4c7782e13e18d0f1012699977989
Ravina <hema130384@gmail.com>
2026-05-12 08:52:30 +0530

    solve que that class with generator which can iterate the umer which are divisible by 7 between a given range 0 to n

--- Python codes/81th_Day.py
+++ Python codes/81th_Day.py
@@ -5,22 +5,15 @@ class DivisibleBySeven:
     def __init__(self,n):
         self.n = n
 
-def generate_divisible_by_seven(self):
-    for num in range(self.n + 1):
-       if num%7 == 0:
+    def generate_divisible_by_seven(self):
+        for num in range(self.n + 1):
+           if num%7 == 0:
             yield num
 
-localhost:8888/notebooks/Piush Kumar Sharma/Basic Python Program.ipynb
-11/26/23, 4:53 AM
-Basic Python Program - Jupyter Notebook
-In [43]:
-1
-2
-3
-4
-5
-6
+
 n = int(input("Enter your desired range: "))
-divisible_by_seven_generator = DivisibleBySeven(n).generate_divisible_b
+
+divisible_by_seven_generator = DivisibleBySeven(n).generate_divisible_by_seven()
+
 for num in divisible_by_seven_generator:
-print(num)
+     print(num)
