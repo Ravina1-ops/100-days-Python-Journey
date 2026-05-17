0704c80e02a8ec154c1ef5a31da25fffc31c99ef
Ravina <hema130384@gmail.com>
2026-05-17 09:51:24 +0530

    write function of taking matirx input and transpsose logic

--- Python codes/84th_Day.py
+++ Python codes/84th_Day.py
@@ -2,13 +2,13 @@
 
 # Function to transpose a matrix
 def transpose_matrix(matrix):
-rows, cols = len(matrix), len(matrix[0])
+     rows, cols = len(matrix), len(matrix[0])
 # Create an empty matrix to store the transposed data
-result = [[0 for _ in range(rows)] for _ in range(cols)]
-for i in range(rows):
-for j in range(cols):
-result[j][i] = matrix[i][j]
-return result
+     result = [[0 for _ in range(rows)] for _ in range(cols)]
+     for i in range(rows):
+         for j in range(cols):
+              result[j][i] = matrix[i][j]
+     return result
 # Input matrix
 matrix = [
     [1, 2, 3],
