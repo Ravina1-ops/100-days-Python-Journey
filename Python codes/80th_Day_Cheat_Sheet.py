74056c6b83023aea207e5c0ad09f6787374e08e8
Ravina <hema130384@gmail.com>
2026-05-25 07:51:36 +0530

    indexing formulas added in cheat sheet

--- Python codes/80th_Day_Cheat_Sheet.py
+++ Python codes/80th_Day_Cheat_Sheet.py
@@ -102,3 +102,14 @@ IndexError - When an index is out of rang
 NameError - When a variable name is not foun
 SyntaxError - When there is an error with how the code is written
 ZeroDivisionError - When your code tries to divide by zero'''
+
+Indexing
+Accessing data from a string, list, or tuple using an element number
+my_string[element_number]
+my_collection[element_number]
+my_tup[element_number]
+Slicing
+Accessing a subset of data from a string, list, or tuple using element numbers from start to stop -1
+my_string[start:stop]
+my_collection[start:stop]
+my_tup[start:stop]
